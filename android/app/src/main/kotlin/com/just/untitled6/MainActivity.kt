bbca2421aeda118a6f4778511f748fc3fea20659
Azizbek <xoliqovazizbek23@gmail.com>
2025-06-27 15:32:23 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/just/untitled6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.just.untitled6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/counter_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class CounterEvent {}
+class Increment extends CounterEvent {}
+class Decrement extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<Increment>((event, emit) => emit(state + 1));
+    on<Decrement>((event, emit) => emit(state - 1));
+  }
+}
